e813c7ebd3b1e8560fafc53d7153c13dcf0e1595
Maxime <maxime.bergerard@hetic.net>
2021-05-14 12:07:15 +0200

    :tada: Init backend and frontend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fa6bb9421947b530afeed8bf31ce6dea'),
+    },
+  },
+});
