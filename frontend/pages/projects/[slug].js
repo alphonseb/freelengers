a77eef0f481c44a8df50424515dc72480e95cf08
alphonseb <alphonse.bouy@gmail.com>
2021-05-15 08:07:53 +0200

    add application

--- frontend/pages/projects/[slug].js
+++ frontend/pages/projects/[slug].js
@@ -1,9 +1,38 @@
+import { useState } from 'react'
 import { fetchAPI } from 'utils/api'
+import Button from 'components/elements/button'
 
-export default function SingleProject (props) {
-    return (
-        <h1>CieWorks</h1>
-    )
+export default function SingleProject ({ name, jobs, id }) {
+    const [selectedJob, setSelectedJob] = useState('')
+    const [dailyRate, setDailyRate] = useState(0)
+    const [daysNumber, setDaysNumber] = useState(0)
+    
+    const apply = async () => {
+        console.log(selectedJob, dailyRate, daysNumber);
+        const createdApplication = await fetchAPI('/applications', { method: 'POST', body: JSON.stringify({job: selectedJob, dailyRate, daysNumber, project: id, user: 1})})
+        console.log(createdApplication);
+    }
+    
+    return name ? (
+        <>
+            <h1>{ name }</h1>
+            <h2>Jobs :</h2>
+            <ul>
+                { jobs.map((job, i) => (<li key={ i }>{ job }</li>))}
+            </ul>
+            <div>
+                <select id="" onChange={(e) => {setSelectedJob(e.target.value)}} value={selectedJob}>
+                    <option value>Choisir</option>
+                    {
+                        jobs.map((job, i) => (<option key={i} value={job}>{ job }</option>))
+                    }
+                </select>
+                <input type="number" name="" id="" value={ dailyRate } onChange={ (e) => {setDailyRate(e.target.value)} }/>
+                <input type="number" name="" id="" value={ daysNumber } onChange={ (e) => {setDaysNumber(e.target.value)} }/>
+            </div>
+            <Button handleClick={apply} button={{text: "Apply"}} />
+        </>
+    ) : (<div>Loading...</div>)
 }
 
 export async function getStaticPaths () {
@@ -12,17 +41,12 @@ export async function getStaticPaths () {
         return strapiProjects
     }
     return fetchProjects().then(projects => {
-        console.log({ projects });
-        return ({ paths: projects.map(project => ({ params: { slug: project.slug, id: project.id } })), fallback: false })
+        return ({ paths: projects.map(project => ({ params: { slug: project.slug } })), fallback: false })
     })
-//     return {
-//         paths: [{ params: { slug: 'cie-works' } }],
-//         fallback: false
-//   }
 }
 
-export async function getStaticProps (params) {
-    console.log(params);
-    return { props: { test: 'test' } }
+export async function getStaticProps ({params}) {
+    const project = await fetchAPI(`/projects/slug/${params.slug}`)
+    return { props: project }
   // Fetch necessary data for the blog post using params.id
 }
