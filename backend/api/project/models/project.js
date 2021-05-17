8b97a0d59688a3e2a4ea89fc9a2091f5c2adf961
alphonseb <alphonse.bouy@gmail.com>
2021-05-18 01:52:13 +0200

    Team creation backend done

--- backend/api/project/models/project.js
+++ backend/api/project/models/project.js
@@ -21,50 +21,69 @@ function cartesian(args) {
     return r;
 }
 
-function createTeams (applications, project) {
+async function createTeams (applications, project) {
+    const algo = await strapi.query('algo').findOne();
     const teams = cartesian(project.jobs.map(job => (applications.filter(application => application.job.id === job.id))))
         .map(team => {
             let green_links = 0
-            let ratings = 0
+            let freelanceRatings = 0
+            let clientRatings = 0
             let jobRatings = 0
             for (const application of team) {
                 const jobRating = (() => {
                     switch (application.job.id) {
-                        case application.user.job_1.id:
+                        case application.user.job_1:
                             return 1
-                        case application.user.job_2.id:
+                        case application.user.job_2:
                             return 0.66
-                        case application.user.job_3.id:
+                        case application.user.job_3:
                             return 0.33
                         default:
                             return 0
-                    }
+                    }   
                 })()
                 jobRatings += jobRating
-                ratings += application.user.rating
+                freelanceRatings += application.user.freelanceRating
+                clientRatings += application.user.clientRating
                 for (let index = 0; index < team.length; index++) {
                     if (application.recommends.find(recommendee => recommendee.id === team[index].user.id)) {
                         green_links += 1
                     }
                 }
             }
+
             const greenAvg = green_links / (team.length * (team.length - 1))
-            const avg = ratings / team.length
+            const freelanceRatingAvg = freelanceRatings / team.length
+            const clientRatingAvg = clientRatings / team.length
+            const jobRatingAvg = jobRatings / team.length
+
+
+            const fullScore = (algo.chemistryCoeff * greenAvg + algo.freelanceRatingCoeff * freelanceRatingAvg + algo.clientRatingCoeff * clientRatingAvg + algo.jobCoeff * jobRatingAvg) / (algo.chemistryCoeff + algo.freelanceRatingCoeff + algo.clientRatingCoeff + algo.jobCoeff)
             
-            team.score = (greenAvg + avg) / 2
-            return team
+            return {
+                applications: team.map(application => application.id),
+                project: project.id,
+                fullScore,
+                jobScore: jobRatingAvg,
+                freelanceRatingScore: freelanceRatingAvg,
+                clientRatingScore: clientRatingAvg,
+                chemistryScore: greenAvg
+            }
         })
-        .sort((a, b) => b.score - a.score)
+        .sort((a, b) => b.fullScore - a.fullScore)
         .slice(0, 3)
-    console.log(teams);
+    return teams
 }
 
 module.exports = {
     lifecycles: {
         async afterUpdate (data) {
             if (data.applicationsOver) {
-                const applications = await strapi.services.applications.find({ project: data.id })
-                createTeams(applications, data)
+                const applications = await strapi.services.application.find({ project: data.id })
+                const teams = await createTeams(applications, data)
+                teams.forEach(team => {
+                    strapi.query('team').create(team)
+                })
             }
         }
     }
