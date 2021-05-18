762b2c65fe6b2904391ce02503e20024944f1c91
Maxime <maxime.bergerard@hetic.net>
2021-05-18 20:47:19 +0200

    Add front login and register page

--- frontend/components/sections/login.js
+++ frontend/components/sections/login.js
@@ -4,6 +4,8 @@ import { useRouter } from "next/router";
 import { login } from "../../lib/auth";
 import AppContext from "context/AppContext";
 
+import ActiveLink from "../01-atoms/ActiveLink";
+
 import ArrowRight from "../../src/assets/icons/arrow-right.svg";
 
 const Login = ({ data }) => {
@@ -25,69 +27,96 @@ const Login = ({ data }) => {
 
   return (
     <section>
-      <h1>Me connecter</h1>
-      <div className='error'>
-        {Object.entries(error).length !== 0 &&
-          error.constructor === Object &&
-          error.message.map((error) => {
-            return (
-              <div key={error.messages[0].id} style={{ marginBottom: 10 }}>
-                <small style={{ color: "red" }}>
-                  {error.messages[0].message}
-                </small>
+      <div className='main-container-login'>
+        <div className='login-image'>
+          <img
+            src='/images/saly-1.png'
+            alt='freelancer illustration'
+          />
+          <img src="/images/saly-2.png" alt="" />
+        </div>
+        <div className='login-form'>
+          <h1>Me connecter</h1>
+          <p className='paragraph'>
+            Vous n’avez pas encore de compte ?&ensp;
+            <ActiveLink href={"/register"} className='accent'>
+              Inscrivez-vous
+            </ActiveLink>
+          </p>
+          <div className='error'>
+            {Object.entries(error).length !== 0 &&
+              error.constructor === Object &&
+              error.message.map((error) => {
+                return (
+                  <div key={error.messages[0].id} style={{ marginBottom: 10 }}>
+                    <small style={{ color: "red" }}>
+                      {error.messages[0].message}
+                    </small>
+                  </div>
+                );
+              })}
+          </div>
+          <form>
+            <fieldset disabled={loading}>
+              <div className='input-container'>
+                <label className='subtitle'>Email</label>
+                <input
+                  type='email'
+                  name='identifier'
+                  onChange={(e) =>
+                    updateUser({ ...user, [e.target.name]: e.target.value })
+                  }
+                  value={user.username}
+                  placeholder='hulkhogan@freelengers.com'
+                />
               </div>
-            );
-          })}
+              <div className='input-container'>
+                <label className='subtitle'>Mot de passe</label>
+                <input
+                  onChange={(e) =>
+                    updateUser({ ...user, [e.target.name]: e.target.value })
+                  }
+                  value={user.password}
+                  type='password'
+                  name='password'
+                  placeholder='*****'
+                />
+              </div>
+              <div className='input-container'>
+                <input
+                  type='submit'
+                  value={loading ? "Chargement.." : "Me connecter"}
+                  disabled={loading}
+                  onClick={() => {
+                    setLoading(true);
+                    login(user.identifier, user.password)
+                      .then((res) => {
+                        setLoading(false);
+                        // set authed User in global context to update header/app state
+                        appContext.setUser(res.data.user);
+                      })
+                      .catch((error) => {
+                        setError(error.response.user);
+                        setLoading(false);
+                      });
+                  }}
+                />
+                <ArrowRight />
+              </div>
+            </fieldset>
+          </form>
+          <p className='paragraph'>
+            Vous avez oublié votre mot de passe ?&ensp;
+            <ActiveLink
+              href={"https://www.youtube.com/watch?v=xvFZjo5PgG0"}
+              className='accent'
+              newTab={true}
+            >
+              Réinitialisez-le
+            </ActiveLink>
+          </p>
+        </div>
       </div>
-      <form>
-        <fieldset disabled={loading}>
-          <div className='input-container'>
-            <label className='subtitle'>Email</label>
-            <input
-              type='email'
-              name='identifier'
-              onChange={(e) =>
-                updateUser({ ...user, [e.target.name]: e.target.value })
-              }
-              value={user.username}
-              placeholder='hulkhogan@freelengers.com'
-            />
-          </div>
-          <div className='input-container'>
-            <label className='subtitle'>Mot de passe</label>
-            <input
-              onChange={(e) =>
-                updateUser({ ...user, [e.target.name]: e.target.value })
-              }
-              value={user.password}
-              type='password'
-              name='password'
-              placeholder='*****'
-            />
-          </div>
-          <div className='input-container'>
-            <input
-              type='submit'
-              value={loading ? "Chargement.." : "Me connecter"}
-              disabled={loading}
-              onClick={() => {
-                setLoading(true);
-                login(user.identifier, user.password)
-                  .then((res) => {
-                    setLoading(false);
-                    // set authed User in global context to update header/app state
-                    appContext.setUser(res.data.user);
-                  })
-                  .catch((error) => {
-                    setError(error.response.user);
-                    setLoading(false);
-                  });
-              }}
-            />
-            <ArrowRight />
-          </div>
-        </fieldset>
-      </form>
     </section>
   );
 };
--- frontend/components/sections/register.js
+++ frontend/components/sections/register.js
@@ -1,12 +1,13 @@
 import React, { useState, useContext } from "react";
 import { registerUser } from "../../lib/auth";
 import AppContext from "../../context/AppContext";
-import { fetchAPI } from 'utils/api'
+import { fetchAPI } from "utils/api";
 import Router from "next/router";
 
+import ActiveLink from "../01-atoms/ActiveLink";
 
 import ArrowRight from "../../src/assets/icons/arrow-right.svg";
-import Button from '../elements/button';
+import Button from "../elements/button";
 
 const Register = ({ data }) => {
   const [newUser, setNewUser] = useState({
@@ -14,185 +15,308 @@ const Register = ({ data }) => {
     username: "",
     password: "",
   });
-  
+
   const [names, setNames] = useState({
     firstName: "",
-    lastName: ""
-  })
-  
-  const [availableJobs, setAvailableJobs] = useState([])
-  const [searchedJob, setSearchedJob] = useState('')
-  
-  const [selectedJobs, setSelectedJobs] = useState([])
-  
-  const [step, setStep] = useState(1)
-  
+    lastName: "",
+  });
+
+  const [availableJobs, setAvailableJobs] = useState([]);
+  const [searchedJob, setSearchedJob] = useState("");
+
+  const [selectedJobs, setSelectedJobs] = useState([]);
+
+  const [step, setStep] = useState(1);
+
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState({});
   const appContext = useContext(AppContext);
-  
+
   const handleRegister = async () => {
     setLoading(true);
-    const userName = `${ names.firstName }_${ names.lastName }#${ Math.floor(Math.random() * 9999) }`
+    const userName = `${names.firstName}_${names.lastName}#${Math.floor(
+      Math.random() * 9999
+    )}`;
     try {
-      const registration = await registerUser(userName, newUser.email, newUser.password)
+      const registration = await registerUser(
+        userName,
+        newUser.email,
+        newUser.password
+      );
       console.log(registration);
-      const user = await fetchAPI(`/users/${ registration.data.user.id }`, { method: 'PUT', body: JSON.stringify(names) })
-      const jobs = await fetchAPI(`/jobs`)
-      setAvailableJobs(jobs)
+      const user = await fetchAPI(`/users/${registration.data.user.id}`, {
+        method: "PUT",
+        body: JSON.stringify(names),
+      });
+      const jobs = await fetchAPI(`/jobs`);
+      setAvailableJobs(jobs);
       appContext.setUser(user);
       setLoading(false);
-      setStep(2)
-      
+      setStep(2);
     } catch (error) {
       setError(error);
-      setLoading(false);  
+      setLoading(false);
     }
-  }
-  
+  };
+
   const selectJob = (job) => {
-      setSearchedJob('')
-      setSelectedJobs([...selectedJobs, {rank: selectedJobs.length + 1, job: job.name ? job : {name: searchedJob}}])
-  }
-  
+    setSearchedJob("");
+    setSelectedJobs([
+      ...selectedJobs,
+      {
+        rank: selectedJobs.length + 1,
+        job: job.name ? job : { name: searchedJob },
+      },
+    ]);
+  };
+
   const removeJob = (job) => {
-    setSelectedJobs(selectedJobs.filter(_job => _job.job.name !== job.job.name).map((_job, i) => ({..._job, rank: i + 1})))
-  }
-  
+    setSelectedJobs(
+      selectedJobs
+        .filter((_job) => _job.job.name !== job.job.name)
+        .map((_job, i) => ({ ..._job, rank: i + 1 }))
+    );
+  };
+
   const sendJobs = async () => {
-    const existingJobs = selectedJobs.filter(job => job.job.id)
-    const newCreatedJobs = await Promise.all(selectedJobs.filter(job => !job.job.id).map(job => fetchAPI('/jobs', {method: 'POST', body:JSON.stringify({name: job.job.name})})))
-    
-    const newJobs = newCreatedJobs.map(job => {
-      return {rank: selectedJobs.find(_job => _job.job.name === job.name).rank, job}
-    })
-    const allJobs = [...existingJobs, ...newJobs]
+    const existingJobs = selectedJobs.filter((job) => job.job.id);
+    const newCreatedJobs = await Promise.all(
+      selectedJobs
+        .filter((job) => !job.job.id)
+        .map((job) =>
+          fetchAPI("/jobs", {
+            method: "POST",
+            body: JSON.stringify({ name: job.job.name }),
+          })
+        )
+    );
+
+    const newJobs = newCreatedJobs.map((job) => {
+      return {
+        rank: selectedJobs.find((_job) => _job.job.name === job.name).rank,
+        job,
+      };
+    });
+    const allJobs = [...existingJobs, ...newJobs];
     const getJobByRank = (rank) => {
-      return allJobs.find(job => job.rank === rank).job.id
-    }
-    
-    const user = await fetchAPI(`/users/${appContext.user.id}`, {method: 'PUT', body: JSON.stringify({job_1: getJobByRank(1), job_2: getJobByRank(2), job_3: getJobByRank(3)})})
-    
+      return allJobs.find((job) => job.rank === rank).job.id;
+    };
+
+    const user = await fetchAPI(`/users/${appContext.user.id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        job_1: getJobByRank(1),
+        job_2: getJobByRank(2),
+        job_3: getJobByRank(3),
+      }),
+    });
+
     if (user) {
-      Router.push('/projects')
+      Router.push("/projects");
     }
-  }
+  };
 
   return (
     <section>
-      <h1>Rejoindre Freelengers</h1>
-      {
-        step === 1 ? (
-          <>
-          {Object.entries(error).length !== 0 &&
-            error.constructor === Object &&
-            error.message.map((error) => {
-              return (
-                <div key={error.messages[0].id} style={{ marginBottom: 10 }}>
-                  <small style={{ color: "red" }}>
-                    {error.messages[0].message}
-                  </small>
-                </div>
-              );
-            })}
-          <form onSubmit={(e) => {e.preventDefault()}}>
-            <fieldset disabled={loading}>
-              <div className='input-container'>
-                <label className='subtitle'>Prénom</label>
-                <input
-                  type='text'
-                  name='firstName'
-                  disabled={loading}
-                  onChange={(e) =>
-                    setNames({ ...names, firstName: e.target.value })
-                  }
-                  value={names.firstName}
-                  placeholder='Clark'
-                />
-              </div>
-              <div className='input-container'>
-                <label className='subtitle'>Nom</label>
-                <input
-                  type='text'
-                  name='lastName'
-                  disabled={loading}
-                  onChange={(e) =>
-                    setNames({ ...names, lastName: e.target.value })
-                  }
-                  value={names.lastName}
-                  placeholder='Kent'
-                />
-              </div>
-              <div className='input-container'>
-                <label className='subtitle'>Email</label>
-                <input
-                  onChange={(e) =>
-                    setNewUser({ ...newUser, email: e.target.value })
-                  }
-                  value={newUser.email}
-                  type='email'
-                  name='email'
-                  placeholder='hulkhogan@freelengers.io'
-                />
-              </div>
-              <div className='input-container'>
-                <label className='subtitle'>Mot de passe</label>
-                <input
-                  onChange={(e) =>
-                    setNewUser({ ...newUser, password: e.target.value })
-                  }
-                  value={newUser.password}
-                  type='password'
-                  name='password'
-                  placeholder='*****'
-                />
-              </div>
-              <div className='input-container'>
-                <input
-                  type='submit'
-                  value={loading ? "Chargement.." : "Je suis input submit"}
-                  disabled={loading}
-                  onClick={handleRegister}
-                />
-                <ArrowRight />
-                <button className='btn-primary' onClick={() => null}>Je suis button <ArrowRight/></button>
-              </div>
-            </fieldset>
-          </form>
-          </>
-        ) : (
-            <div>
-              <h2>Que faites-vous ?</h2>
-              <input type="search" value={ searchedJob } onChange={ (e) => { setSearchedJob(e.target.value) } } />
-              <ul>
-              {
-                availableJobs.filter(job => job.name.toLowerCase().includes(searchedJob.toLowerCase())).length ? (
-                    availableJobs.filter(job => searchedJob ? job.name.toLowerCase().includes(searchedJob.toLowerCase()) : job).map(job => (
-                      <li key={job.id}><span>{ job.name }</span><Button disabled={selectedJobs.length === 3 || selectedJobs.find(_job => _job.job.name === job.name)} button={{text: 'Sélectionner'}} handleClick={() => {selectJob(job)}} /></li>
-                  ))
-                  ) : (
-                      <>
-                        {
-                          searchedJob ? (
-                            <li><span>{ searchedJob }</span><Button disabled={selectedJobs.length === 3 || selectedJobs.find(job => job.job.name === searchedJob)} button={{text: 'Sélectionner'}} handleClick={selectJob} /></li>
-                          ) : ''
+      {step === 1 ? (
+        <>
+          <div className='main-container-register'>
+            <h1>Rejoindre Freelengers</h1>
+            <div className='register-background'></div>
+            <div className='register-form'>
+              <h1>Rejoindre Freelengers</h1>
+              <p className='paragraph'>
+                Vous faites déjà parti de notre communauté ?&ensp;
+                <ActiveLink href={"/register"} className='accent'>
+                  Connectez-vous
+                </ActiveLink>
+              </p>
+              {Object.entries(error).length !== 0 &&
+                error.constructor === Object &&
+                error.message.map((error) => {
+                  return (
+                    <div
+                      key={error.messages[0].id}
+                      style={{ marginBottom: 10 }}
+                    >
+                      <small style={{ color: "red" }}>
+                        {error.messages[0].message}
+                      </small>
+                    </div>
+                  );
+                })}
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                <fieldset disabled={loading}>
+                  <div className='input-container'>
+                    <label className='subtitle'>Prénom</label>
+                    <input
+                      type='text'
+                      name='firstName'
+                      disabled={loading}
+                      onChange={(e) =>
+                        setNames({ ...names, firstName: e.target.value })
+                      }
+                      value={names.firstName}
+                      placeholder='Clark'
+                    />
+                  </div>
+                  <div className='input-container'>
+                    <label className='subtitle'>Nom</label>
+                    <input
+                      type='text'
+                      name='lastName'
+                      disabled={loading}
+                      onChange={(e) =>
+                        setNames({ ...names, lastName: e.target.value })
+                      }
+                      value={names.lastName}
+                      placeholder='Kent'
+                    />
+                  </div>
+                  <div className='input-container'>
+                    <label className='subtitle'>Email</label>
+                    <input
+                      onChange={(e) =>
+                        setNewUser({ ...newUser, email: e.target.value })
+                      }
+                      value={newUser.email}
+                      type='email'
+                      name='email'
+                      placeholder='hulkhogan@freelengers.io'
+                    />
+                  </div>
+                  <div className='input-grid-2'>
+                    <div className='input-container'>
+                      <label className='subtitle'>Mot de passe</label>
+                      <input
+                        onChange={(e) =>
+                          setNewUser({ ...newUser, password: e.target.value })
                         }
-                      </>
-                    )
-              }
-              </ul>
-              <h3>Mes métiers</h3>
-              <ol>
-                {
-                  selectedJobs.map((job, i) => (
-                    <li key={ i }><span>{ job.job.name }</span><Button button={{text: "Retirer"}} handleClick={() => {removeJob(job)}} /></li>
-                  ))
-                }
-              </ol>
-              <Button disabled={selectedJobs.length < 3} button={{text: "Valider"}} handleClick={sendJobs} />
+                        value={newUser.password}
+                        type='password'
+                        name='password'
+                        placeholder='*****'
+                      />
+                    </div>
+                    <div className='input-container'>
+                      <label className='subtitle'>
+                        Confirmez le mot de passe
+                      </label>
+                      <input
+                        onChange={(e) =>
+                          setNewUser({ ...newUser, password: e.target.value })
+                        }
+                        value={newUser.password}
+                        type='password'
+                        name='password'
+                        placeholder='*****'
+                      />
+                    </div>
+                  </div>
+                  <div className='input-container-checkbox'>
+                    <input type='checkbox' name='CGU' />
+                    <p className='paragraph'>
+                      En créant mon compte j’accepte les Conditions générales
+                      d’utilisation et la politique de confidentialité
+                    </p>
+                  </div>
+                  <div className='input-container'>
+                    <input
+                      type='submit'
+                      value={loading ? "Chargement.." : "Je suis input submit"}
+                      disabled={loading}
+                      onClick={handleRegister}
+                    />
+                    <ArrowRight />
+                    <button className='btn-primary' onClick={() => null}>
+                      Je suis button <ArrowRight />
+                    </button>
+                  </div>
+                </fieldset>
+              </form>
             </div>
-        )
-      }
+          </div>
+        </>
+      ) : (
+        <div>
+          <h2>Que faites-vous ?</h2>
+          <input
+            type='search'
+            value={searchedJob}
+            onChange={(e) => {
+              setSearchedJob(e.target.value);
+            }}
+          />
+          <ul>
+            {availableJobs.filter((job) =>
+              job.name.toLowerCase().includes(searchedJob.toLowerCase())
+            ).length ? (
+              availableJobs
+                .filter((job) =>
+                  searchedJob
+                    ? job.name.toLowerCase().includes(searchedJob.toLowerCase())
+                    : job
+                )
+                .map((job) => (
+                  <li key={job.id}>
+                    <span>{job.name}</span>
+                    <Button
+                      disabled={
+                        selectedJobs.length === 3 ||
+                        selectedJobs.find((_job) => _job.job.name === job.name)
+                      }
+                      button={{ text: "Sélectionner" }}
+                      handleClick={() => {
+                        selectJob(job);
+                      }}
+                    />
+                  </li>
+                ))
+            ) : (
+              <>
+                {searchedJob ? (
+                  <li>
+                    <span>{searchedJob}</span>
+                    <Button
+                      disabled={
+                        selectedJobs.length === 3 ||
+                        selectedJobs.find((job) => job.job.name === searchedJob)
+                      }
+                      button={{ text: "Sélectionner" }}
+                      handleClick={selectJob}
+                    />
+                  </li>
+                ) : (
+                  ""
+                )}
+              </>
+            )}
+          </ul>
+          <h3>Mes métiers</h3>
+          <ol>
+            {selectedJobs.map((job, i) => (
+              <li key={i}>
+                <span>{job.job.name}</span>
+                <Button
+                  button={{ text: "Retirer" }}
+                  handleClick={() => {
+                    removeJob(job);
+                  }}
+                />
+              </li>
+            ))}
+          </ol>
+          <Button
+            disabled={selectedJobs.length < 3}
+            button={{ text: "Valider" }}
+            handleClick={sendJobs}
+          />
+        </div>
+      )}
     </section>
   );
 };
