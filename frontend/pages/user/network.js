01da182a91dfa03bb68948237bbe9dffa86aa77c
Maxime <maxime.bergerard@hetic.net>
2021-05-21 01:50:30 +0200

    finish network

--- frontend/pages/user/network.js
+++ frontend/pages/user/network.js
@@ -1,6 +1,5 @@
 import appContext from "context/AppContext";
 import { useContext, useEffect, useState } from "react";
-import { useRouter } from "next/router";
 
 import { fetchAPI } from "utils/api";
 
@@ -8,13 +7,14 @@ import ArrowRight from "../../src/assets/icons/arrow-right.svg";
 import Search from "../../src/assets/icons/searchicon.svg";
 import UserAdd from "../../src/assets/icons/user-plus.svg";
 import UserPending from "../../src/assets/icons/user-pending.svg";
-import UserMinus from "../../src/assets/icons/user-minus.svg";
+import Check from "../../src/assets/icons/check.svg";
+import Delete from "../../src/assets/icons/delete.svg";
+// import UserMinus from "../../src/assets/icons/user-minus.svg";
 
 import Button from "components/elements/button";
 
 export default function Network() {
   const { user } = useContext(appContext);
-  // console.log(user);
 
   const [receivedRequests, setReceivedRequests] = useState([]);
   const [sentRequests, setSentRequests] = useState([]);
@@ -27,8 +27,12 @@ export default function Network() {
   const [friend, setFriend] = useState(null);
   const [friendError, setFriendError] = useState("");
 
-  const nav = ["Mon réseau", "Demandes envoyées", "Demandes reçues"];
-  const [navNetwork, setNavNetwork] = useState(nav[0]);
+  const nav = [
+    { name: "Mon réseau" },
+    { name: "Demandes envoyées" },
+    { name: "Demandes reçues" },
+  ];
+  const [navNetwork, setNavNetwork] = useState(0);
 
   const handleSearch = async (e) => {
     if (e.key === "Enter" || e === "Icon") {
@@ -42,6 +46,19 @@ export default function Network() {
       }
     }
   };
+
+  const filterFriends = (friends, searchUsername) => {
+    if (!searchUsername) {
+      return friends;
+    }
+
+    return friends.filter((friend) => {
+      const friendName = friend.username.toLowerCase();
+      return friendName.includes(searchUsername.toLowerCase());
+    });
+  };
+
+  const filteredFriends = filterFriends(friends, searchUsername);
 
   useEffect(() => {
     async function fetchRequests() {
@@ -87,173 +104,200 @@ export default function Network() {
   return user ? (
     <div className='network-main-container'>
       <div className='network-nav'>
-          {/* {nav.map(item => {
-            <p>{item}</p>
-          })} */}
-          {nav}
         <ul>
-          <li>zdefgh</li>
+          {nav.map((item, i) => (
+            <li
+              key={i}
+              className={navNetwork === i ? "isSelected" : null}
+              onClick={() => {
+                setNavNetwork(i);
+              }}
+            >
+              {item.name}
+            </li>
+          ))}
         </ul>
       </div>
-      <div className='my-network'>
-        <div className='input-container'>
-          <input
-            type='search'
-            value={searchUsername}
-            onChange={(e) => {
-              setSearchUsername(e.target.value);
-              setFriendError("");
-            }}
-            onKeyDown={(e) => {
-              handleSearch(e);
-            }}
-            placeholder='Rechercher un freelenger'
-          />
-          <Search onClick={() => handleSearch("Icon")} />
-        </div>
-        {friend && searchUsername ? (
-          <ul className='my-friends'>
-            <li key={friend.id}>
-              <div className='friend-picture' />
-              <div className='friend-infos'>
-                <p>
-                  {friend.firstName} {friend.lastName}
-                </p>
-              </div>
-              <FriendAction
-                status={friend.status}
-                userId={user.id}
-                friendId={friend.id}
-              />
-            </li>
-          </ul>
-        ) : (
-          <>
-            {friendError ? (
-              <div className='no-network'>
-                <div className='no-network-texts'>
-                  <h3>Il semble que cet utilisateur n’existe pas ! </h3>
-                  <p className='paragraph'>
-                    Il y a une erreur avec le nom d’utilisateur. Cela peut
-                    provenir d’une faute d’orthographe.
+      {navNetwork === 0 ? (
+        <div className='my-network'>
+          <div className='input-container'>
+            <input
+              type='search'
+              value={searchUsername}
+              onChange={(e) => {
+                setSearchUsername(e.target.value);
+                setFriendError("");
+              }}
+              onKeyDown={(e) => {
+                handleSearch(e);
+              }}
+              placeholder='Rechercher un freelenger'
+            />
+            <Search onClick={() => handleSearch("Icon")} />
+          </div>
+          {friend && searchUsername ? (
+            <ul className='my-friends'>
+              <li key={friend.id}>
+                <div className='friend-picture' />
+                <div className='friend-infos'>
+                  <p>
+                    {friend.firstName} {friend.lastName}
                   </p>
                 </div>
-                <img src='/images/friendError.png' alt='' />
-              </div>
-            ) : (
-              <>
-                {friends.length ? (
-                  <ul className='my-friends'>
-                    {friends.map((friend) => (
-                      <li key={friend.id}>
-                        <div className='friend-picture' />
-                        <div className='friend-infos'>
-                          <p>
-                            {friend.firstName} {friend.lastName}
-                          </p>
-                          <div className='friend-job'>
-                            <span>{friend.job_1.name}</span>
-                            <span>{friend.job_2.name}</span>
-                            <span>{friend.job_3.name}</span>
-                          </div>
-                        </div>
-                        {/* <div className='remove'>
-                          <UserMinus />
-                        </div> */}
-                      </li>
-                    ))}
-                  </ul>
-                ) : (
-                  <div className='no-network'>
-                    <div className='no-network-texts'>
-                      <h3>
-                        Quel dommage, tu n’as pas encore de réseau pour
-                        l’instant !
-                      </h3>
-                      <p className='paragraph'>
-                        Rejoins notre communauté sur circle, et rencontre des
-                        freelengers chauds près de chez toi !
-                      </p>
-                      <button
-                        className='btn-primary'
-                        onClick={() =>
-                          window.open(
-                            "https://freelengers.circle.so/home",
-                            "_blank"
-                          )
-                        }
-                      >
-                        Découvre notre communauté ! <ArrowRight />
-                      </button>
-                    </div>
-                    <img src='/images/mynetwork.png' alt='' />
+                <FriendAction
+                  status={friend.status}
+                  userId={user.id}
+                  friendId={friend.id}
+                />
+              </li>
+            </ul>
+          ) : (
+            <>
+              {friendError ? (
+                <div className='no-network'>
+                  <div className='no-network-texts'>
+                    <h3>Il semble que cet utilisateur n’existe pas ! </h3>
+                    <p className='paragraph'>
+                      Il y a une erreur avec le nom d’utilisateur. Cela peut
+                      provenir d’une faute d’orthographe.
+                    </p>
                   </div>
-                )}
-              </>
-            )}
-          </>
-        )}
-      </div>
-      {/* <h1>My Network</h1>
-
-      <h2>Pending Requests</h2>
-      {receivedRequests.length ? (
-        <ul>
-          {receivedRequests.map((req) => (
-            <li key={req.id}>
-              <span>
-                From {req.requester.username} - {req.requester.firstName}{" "}
-                {req.requester.lastName}
-              </span>
-              {requestStatus ? (
-                <span>{requestStatus}</span>
+                  <img src='/images/friendError.png' alt='' />
+                </div>
               ) : (
                 <>
-                  <Button
-                    button={{ text: "Accept" }}
-                    handleClick={() => {
-                      acceptRequest(req.requester);
-                    }}
-                  />
-                  <Button
-                    button={{ text: "Decline" }}
-                    handleClick={() => {
-                      denyRequest(req.requester);
-                    }}
-                  />
+                  {filteredFriends.length ? (
+                    <ul className='my-friends'>
+                      {filteredFriends.map((friend) => (
+                        <li key={friend.id}>
+                          <div className='friend-picture' />
+                          <div className='friend-infos'>
+                            <p>
+                              {friend.firstName} {friend.lastName}
+                            </p>
+                            <div className='friend-job'>
+                              <span>{friend.job_1.name}</span>
+                              <span>{friend.job_2.name}</span>
+                              <span>{friend.job_3.name}</span>
+                            </div>
+                          </div>
+                          {/* <div className='remove'>
+                          <UserMinus />
+                        </div> */}
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <div className='no-network'>
+                      <div className='no-network-texts'>
+                        <h3>
+                          Quel dommage, tu n’as pas encore de réseau pour
+                          l’instant !
+                        </h3>
+                        <p className='paragraph'>
+                          Rejoins notre communauté sur circle, et rencontre des
+                          freelengers chauds près de chez toi !
+                        </p>
+                        <button
+                          className='btn-primary'
+                          onClick={() =>
+                            window.open(
+                              "https://freelengers.circle.so/home",
+                              "_blank"
+                            )
+                          }
+                        >
+                          Découvre notre communauté ! <ArrowRight />
+                        </button>
+                      </div>
+                      <img src='/images/mynetwork.png' alt='' />
+                    </div>
+                  )}
                 </>
               )}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        ""
-      )}
-      <h2>Sent Requests</h2>
-      {sentRequests.length ? (
-        <ul>
-          {sentRequests.map((req) => (
-            <li key={req.id}>
-              <span>
-                To {req.requestee.username} - {req.requestee.firstName}{" "}
-                {req.requestee.lastName}
-              </span>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        ""
-      )}
-      <h2>My Network</h2>
-      {friends.length ? (
-        <ul>
-          {friends.map((friend) => (
-            <li key={friend.id}>{friend.username}</li>
-          ))}
-        </ul>
-      ) : (
-        ""
-      )} */}
+            </>
+          )}
+        </div>
+      ) : null}
+      {navNetwork === 1 ? (
+        <div className='my-network'>
+          <h3>Demande en attente de validation</h3>
+          {sentRequests.length ? (
+            <ul className='my-friends'>
+              {sentRequests.map((req) => (
+                <li key={req.id}>
+                  <div className='friend-picture' />
+                  <div className='friend-infos'>
+                    <p>
+                      {req.requestee.firstName} {req.requestee.lastName}
+                    </p>
+                  </div>
+                  <div className='request-sent'>
+                    <UserPending />
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : null}
+        </div>
+      ) : null}
+      {navNetwork === 2 ? (
+        <div className='my-network'>
+          <h3>Les Freelengers qui veulent rejoindre votre réseau</h3>
+          {receivedRequests.length ? (
+            <>
+              {requestStatus ? (
+                <div className='modal'>
+                  <img src='/images/requestSent.png' alt='' />
+                  <h3>
+                    {requestStatus === "Request denied."
+                      ? "Demande refusée"
+                      : "Demande acceptée"}
+                  </h3>
+                  <button
+                    className='btn-primary'
+                    onClick={() => {
+                      setRequestStatus("");
+                      setNavNetwork(0);
+                    }}
+                  >
+                    Retourner à mon réseau
+                  </button>
+                </div>
+              ) : (
+                <ul className='my-friends'>
+                  {receivedRequests.map((req) => (
+                    <li key={req.id}>
+                      <div className='friend-picture' />
+                      <div className='friend-infos'>
+                        <p>
+                          {req.requester.firstName} {req.requester.lastName}
+                        </p>
+                      </div>
+                      {requestStatus ? null : (
+                        <>
+                          <div className='pending-invit'>
+                            <Check
+                              onClick={() => {
+                                acceptRequest(req.requester);
+                              }}
+                            />
+                            <Delete
+                              onClick={() => {
+                                denyRequest(req.requester);
+                              }}
+                            />
+                          </div>
+                        </>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </>
+          ) : null}
+        </div>
+      ) : null}
     </div>
   ) : null;
 }
